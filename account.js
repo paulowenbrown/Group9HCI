5c84cabf607d8e2a2381f87f9481c08ff2838960
dntrng <pdtrang18899@gmail.com>
2020-11-11 10:12:24 -0600

    html and js file

---
+++ account.js
@@ -0,0 +1,64 @@
+$("#contacts").hide();
+$("#cards").hide();
+$("#addresses").hide();
+
+$("#account-profile-tab").click(function() {
+	$("#profile").show();
+	$("#contacts").hide();
+	$("#cards").hide();
+	$("#addresses").hide();
+});
+
+$("#account-card-tab").click(function() {
+	$("#profile").hide();
+	$("#contacts").hide();
+	$("#cards").show();
+	$("#addresses").hide();
+});
+
+$("#account-contact-tab").click(function() {
+	$("#profile").hide();
+	$("#contacts").show();
+	$("#cards").hide();
+	$("#addresses").hide();
+});
+
+$("#account-address-tab").click(function() {
+	$("#profile").hide();
+	$("#contacts").hide();
+	$("#cards").hide();
+	$("#addresses").show();
+});
+
+var spanClose = $(".close");
+
+$("#btnAddress").click(function() {
+ 	$("#add-address-modal").show();
+});
+
+$("#btnPwd").click(function() {
+ 	$("#change-pwd-modal").show();
+});
+
+$("#btnContact").click(function() {
+ 	$("#add-contact-modal").show();
+});
+
+$("#btnCard").click(function() {
+ 	$("#add-card-modal").show();
+});
+
+$(".submit").click(function() {
+  	var r = confirm("Are you sure you want to add this information?");
+  	if (r == true) {
+    	$(".modal").hide();
+ 	}
+});
+
+$("#change-pwd").click(function() {
+	$(".modal").hide();
+});
+
+spanClose.click(function() {
+  	$(".modal").hide();
+});
